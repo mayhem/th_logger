5ce7e2ba8aa7c581042d529c1b9d641018ec9287
Robert Kaye <rob@musicbrainz.org>
2014-09-20 16:05:15 +0000

    Add the crucial info into the title of the page. Check in favicon

--- flask/server.py
+++ flask/server.py
@@ -2,17 +2,25 @@
 
 import subprocess
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__,
+            static_url_path = "/static",
+            static_folder = "static",
+            template_folder = "template")
 
 @app.route('/')
 def index():
-    data = subprocess.check_output(["th_logger"])
+    while True:
+        try:
+            data = subprocess.check_output(["th_logger"])
+        except subprocess.CalledProcessError:
+            continue
+        break
     temp, hum = data.split(",")
     temp_F = float(temp) * 1.8 + 32
-    return '''<html><head><meta http-equiv="refresh" content="60"></head><body>
+    return '''<html><head><title>%sC/%s%%</title><link rel="shortcut icon" href="/static/images/favicon.gif"><meta http-equiv="refresh" content="60"></head><body>
            <div style="text-align: center; font-family: Arial, Helvetica, sans-serif; font-size: 35pt">
-           %s C / %.1f F<br>%s%% humidity</div></body></html>''' % (temp, temp_F, hum)
+           %s C / %.1f F<br>%s%% humidity</div></body></html>''' % (temp, hum, temp, temp_F, hum)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
